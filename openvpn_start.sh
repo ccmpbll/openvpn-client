3207f7d83fea199954486e330c898d3c9b4826a9
Chris Campbell <ccmpbll@users.noreply.github.com>
2019-04-12 23:35:55 -0400

    Create openvpn_start.sh
    creating new entrypoint file, trying this a different way...

---
+++ openvpn_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Check Env for config file name
+if [[ "${OPENVPN_CONF}" == "NONE" ]]; then
+  echo "OpenVPN config not provided. Exiting."
+  exit 1
+fi  
+
+#Check if config file exists
+if [[ -f "${CONFIG_PATH}/${OPENVPN_CONF}" ]]; then
+  echo "Using supplied OpenVPN config: ${CONFIG_PATH}/${OPENVPN_CONF}"
+else
+  echo "Supplied OpenVPN config ${CONFIG_PATH}/${OPENVPN_CONF} could not be found."
+  exit 1
+fi
+
+#Check Env for auth file name
+if [[ "${OPENVPN_AUTH}" == "NONE" ]]; then
+  echo "OpenVPN auth not provided. Exiting."
+  exit 1
+fi  
+
+#Check if config file exists
+if [[ -f "${CONFIG_PATH}/${OPENVPN_AUTH}" ]]; then
+  echo "Using supplied OpenVPN auth file: ${CONFIG_PATH}/${OPENVPN_AUTH}"
+else
+  echo "Supplied OpenVPN auth file ${CONFIG_PATH}/${OPENVPN_AUTH} could not be found."
+  exit 1
+fi
+
+exec openvpn --config "${CONFIG_PATH}/${OPENVPN_CONF}" --auth-user-pass "${CONFIG_PATH}/${OPENVPN_AUTH}" --auth-nocache ${OPENVPN_OPTS}
